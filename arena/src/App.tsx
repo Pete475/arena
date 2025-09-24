2ddbeaf5c13b10915c5414bb204598652af7b8d9
Pete475 <90112229+Pete475@users.noreply.github.com>
2025-09-23 16:14:43 -1000

    front end component titles added to main

--- arena/src/App.tsx
+++ arena/src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom'; 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './Login';
-import Dashboard from './Dashboard'; 
+import Dashboard from './Dashboard';
 
 function App() {
   return (
     <div>
+      <p>hello world</p>
       <Routes>
         <Route path='/' element={<Login />} />
         <Route path='/dashboard' element={<Dashboard />} />
       </Routes>
-      <p>hello world</p>
     </div>
   );
 }
