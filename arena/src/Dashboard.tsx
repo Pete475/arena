2d03cdb25d055118afcb0a116a968614790f03ff
Pete475 <90112229+Pete475@users.noreply.github.com>
2025-10-01 15:01:31 -1000

    dashboard component

--- arena/src/Dashboard.tsx
+++ arena/src/Dashboard.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import ContestList from './DashboardComponents/ContestList';
+import ImageFeed from './DashboardComponents/ImageFeed'
 function Dashboard() {
-  return <></>;
+  return (
+    <div>
+      <ContestList />
+      <ImageFeed />
+    </div>
+  );
 }
 export default Dashboard;
